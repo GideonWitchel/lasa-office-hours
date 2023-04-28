c6feecb79b7afef8a05da1ea5e8001e5d9b8c663
Gideon Witchel <94641536+GideonWitchel@users.noreply.github.com>
2023-04-28 14:16:06 -0500

    Formatting python script

---
+++ website/index.js
@@ -0,0 +1,20 @@
+
+classes = [[]]
+data = [[[]]]
+
+buttonHTML1 = "<button type=\"button\" class=\"block btn btn-primary\" data-toggle=\"modal\" data-target=\"#classInfo\">tmp"
+buttonHTML2 = "</button>\n"
+
+window.addEventListener("load", function() {
+    drawButtons()
+});
+
+function drawButtons() {
+    for (let day = 0; day < 5; day++) {
+        for (let time = 0; time < 3; time++){
+            for(let classNum = 0; class < data[day][time].length; classNum++) {
+
+            }
+        }
+    }
+}
