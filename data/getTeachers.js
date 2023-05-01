6e751f3c10e9361eab36c6d2d34b6bbc0eaadbd4
Gideon Witchel <94641536+GideonWitchel@users.noreply.github.com>
2023-05-01 13:42:28 -0500

    Script to get teachers from Frontline

---
+++ data/getTeachers.js
@@ -0,0 +1,35 @@
+// Open SIS Frontline
+// Navigate to the "Report Card" pane
+// Run the script
+
+// This does not load, store, or in any way affect permanent TEAMS data
+// This functions entirely locally on your machine, not contacting any external servers
+// This is only meant to make listing teachers easier
+
+function getTeachers() {
+	let teachers = new Set()
+	let classIndex = 2
+
+	// Arbitrary stop of 100 in case error catching fails
+	while (classIndex < 100) {
+		try {
+			teacher = document.getElementById("r"+classIndex+"td2").firstElementChild.innerText
+			teachers.add(teacher)
+		}
+		catch(err) {
+			break
+		}
+		classIndex++
+	}
+	
+	return teachers
+}
+
+async function displaySet(teachers){
+	// Output in every way possible
+	console.log(teachers)
+	document.body.innerHTML = (Array.from(teachers).join(";")+"<br><br><br><br>Reload to bring back normal TEAMS.")
+	await navigator.clipboard.writeText(teachers);
+}
+
+displaySet(getTeachers())
